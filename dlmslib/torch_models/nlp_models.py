38f5ee21006bbbf59bda4065c54a4d1e727c0632
KeyiT <keyit92@gmail.com>
2019-02-05 22:45:33 -0800

    develop: update pytorch model

--- dlmslib/torch_models/nlp_models.py
+++ dlmslib/torch_models/nlp_models.py
@@ -1,5 +1,6 @@
 import collections
 
+import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import autograd
@@ -9,22 +10,36 @@ from torch import nn
 
 class RNTN(nn.Module):
 
-    def __init__(self, word2index, hidden_size, output_size, use_gpu=False):
+    def __init__(self, word2index, embed_matrix, output_size, trainable_embed=True, use_gpu=False):
         super(RNTN, self).__init__()
 
+        self.word2index = word2index
+        self.trainable_embed = trainable_embed
         self.use_gpu = use_gpu
 
-        self.word2index = word2index
-        self.embed = nn.Embedding(len(word2index), hidden_size)
+        if isinstance(embed_matrix, np.ndarray):
+            voc_size, hidden_size = embed_matrix.shape
+            self.embed = nn.Embedding(voc_size, hidden_size)
+            self.embed.load_state_dict({'weight': embed_matrix})
+            self.embed.weight.requires_grad = trainable_embed
+        elif isinstance(embed_matrix, (int, np.int8, np.int16, np.int32, np.int64, np.int128)):
+            hidden_size = embed_matrix
+            voc_size = len(word2index)
+            self.embed = nn.Embedding(voc_size, hidden_size)
+            self.embed.weight.requires_grad = trainable_embed
+        else:
+            raise ValueError("embed matrix must be either 2d numpy array or integer")
+
         self.V = nn.ParameterList(
             [nn.Parameter(torch.randn(hidden_size * 2, hidden_size * 2)) for _ in range(hidden_size)])  # Tensor
         self.W = nn.Parameter(torch.randn(hidden_size * 2, hidden_size))
         self.b = nn.Parameter(torch.randn(1, hidden_size))
-        #         self.W_out = nn.Parameter(torch.randn(hidden_size,output_size))
         self.W_out = nn.Linear(hidden_size, output_size)
 
     def init_weight(self):
-        nn.init.xavier_uniform(self.embed.state_dict()['weight'])
+        if self.trainable_embed:
+            nn.init.xavier_uniform(self.embed.state_dict()['weight'])
+
         nn.init.xavier_uniform(self.W_out.state_dict()['weight'])
         for param in self.V.parameters():
             nn.init.xavier_uniform(param)
@@ -36,9 +51,8 @@ class RNTN(nn.Module):
         LongTensor = tcuda.LongTensor if self.use_gpu else torch.LongTensor
 
         recursive_tensor = collections.OrderedDict()
-        if node.isLeaf:
-            tensor = autograd.Variable(LongTensor([self.word2index[node.word]])) if node.word in self.word2index.keys() \
-                else autograd.Variable(LongTensor([self.word2index['<UNK>']]))
+        if node.is_leaf():
+            tensor = autograd.Variable(LongTensor([self.word2index[node.text]]))
             current = self.embed(tensor)  # 1xD
         else:
             recursive_tensor.update(self.tree_propagation(node.left))
@@ -58,16 +72,16 @@ class RNTN(nn.Module):
         recursive_tensor[node] = current
         return recursive_tensor
 
-    def forward(self, trees, root_only=False):
+    def forward(self, tree_roots, root_only=False):
 
         propagated = []
-        if not isinstance(trees, list):
-            trees = [trees]
+        if not isinstance(tree_roots, list):
+            tree_roots = [tree_roots]
 
-        for Tree in trees:
-            recursive_tensor = self.tree_propagation(Tree.root)
+        for tree_root in tree_roots:
+            recursive_tensor = self.tree_propagation(tree_root)
             if root_only:
-                recursive_tensor = recursive_tensor[Tree.root]
+                recursive_tensor = recursive_tensor[tree_root]
                 propagated.append(recursive_tensor)
             else:
                 recursive_tensor = [tensor for node, tensor in recursive_tensor.items()]
--- dlmslib/torch_models/trees.py
+++ dlmslib/torch_models/trees.py
@@ -1,100 +1,64 @@
-
-
 class LabeledTextBinaryTreeNode(object):  # a node in the tree
-    def __init__(self, label, word=None):
+    def __init__(self, label, text=None):
         self.label = label
-        self.word = word
-        self.parent = None  # reference to parent
+        self.text = text
         self.left = None  # reference to left child
         self.right = None  # reference to right child
-        # true if I am a leaf (could have probably derived this from if I have
-        # a word)
-        self.isLeaf = False
+
+    def __str__(self):
+        if self.is_leaf():
+            return '[{0}:{1}]'.format(self.text, self.label)
+        return '({0} <- [{1}:{2}] -> {3})'.format(self.left, self.text, self.label, self.right)
+
+    def is_leaf(self):
         # true if we have finished performing fowardprop on this node (note,
         # there are many ways to implement the recursion.. some might not
         # require this flag)
+        return self.left is None and self.right is None
 
-    def __str__(self):
-        if self.isLeaf:
-            return '[{0}:{1}]'.format(self.word, self.label)
-        return '({0} <- [{1}:{2}] -> {3})'.format(self.left, self.word, self.label, self.right)
+    def get_leaf_texts(self):
+        # from left to right
+        if self.is_leaf():
+            return [self.text]
+        else:
+            return self.get_leaf_texts(self.left) + self.get_leaf_texts(self.right)
 
-
-class LabeledTextBinaryTree(object):
-
-    def __init__(self, tree_string, open_char='(', close_char=')'):
+    @classmethod
+    def parse_ptb_string(cls, ptb_string, open_char='(', close_char=')'):
         tokens = []
-        self.open = open_char
-        self.close = close_char
-        for toks in tree_string.strip().split():
+        for toks in ptb_string.strip().split():
             tokens += list(toks)
-        self.root = self.parse(tokens)
-        # get list of labels as obtained through a post-order traversal
-        self.labels = get_labels(self.root)
-        self.num_words = len(self.labels)
+        return LabeledTextBinaryTreeNode.__parse_ptb_tokens(tokens, open_char=open_char, close_char=close_char)
 
-    def parse(self, tokens, parent=None):
-        assert tokens[0] == self.open, "Malformed tree"
-        assert tokens[-1] == self.close, "Malformed tree"
+    @classmethod
+    def __parse_ptb_tokens(cls, tokens, open_char='(', close_char=')'):
+        assert tokens[0] == open_char, "Malformed tree"
+        assert tokens[-1] == close_char, "Malformed tree"
 
         split = 2  # position after open and label
         count_open = count_close = 0
 
-        if tokens[split] == self.open:
+        if tokens[split] == open_char:
             count_open += 1
             split += 1
         # Find where left child and right child split
         while count_open != count_close:
-            if tokens[split] == self.open:
+            if tokens[split] == open_char:
                 count_open += 1
-            if tokens[split] == self.close:
+            if tokens[split] == close_char:
                 count_close += 1
             split += 1
 
         # New node
         node = LabeledTextBinaryTreeNode(int(tokens[1]))  # zero index labels
 
-        node.parent = parent
-
         # leaf Node
         if count_open == 0:
-            node.word = ''.join(tokens[2: -1]).lower()  # lower case?
+            node.text = ''.join(tokens[2: -1]).lower()  # lower case?
             node.isLeaf = True
             return node
 
-        node.left = self.parse(tokens[2: split], parent=node)
-        node.right = self.parse(tokens[split: -1], parent=node)
+        node.left = LabeledTextBinaryTreeNode.__parse_ptb_tokens(tokens[2: split], parent=node)
+        node.right = LabeledTextBinaryTreeNode.__parse_ptb_tokens(tokens[split: -1], parent=node)
 
         return node
-
-    def get_words(self):
-        leaves = get_leaves(self.root)
-        words = [node.word for node in leaves]
-        return words
-
-
-def get_labels(node):
-    if node is None:
-        return []
-    return get_labels(node.left) + get_labels(node.right) + [node.label]
-
-
-def get_leaves(node):
-    if node is None:
-        return []
-    if node.isLeaf:
-        return [node]
-    else:
-        return get_leaves(node.left) + get_leaves(node.right)
-
-
-def load_trees(dataSet='train'):
-    """
-    Loads training trees. Maps leaf node words to word ids.
-    """
-    file = '../dataset/trees/%s.txt' % dataSet
-    print("Loading %s trees.." % dataSet)
-    with open(file, 'r', encoding='utf-8') as fid:
-        trees = [LabeledTextBinaryTree(l) for l in fid.readlines()]
-
-    return trees
